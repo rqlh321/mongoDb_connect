cd20ecbaa7e45f81a4324bd399826bb66912f000
rqlh321 <rqlh321@gmail.com>
2017-01-06 22:22:46 +0300

    hard code test

--- src/main/java/MongoDb.java
+++ src/main/java/MongoDb.java
@@ -9,12 +9,12 @@ public class MongoDb {
     public static Datastore getInstance() {
         if (datastore == null) {
             System.out.println("MongoDb connecting...");
-            MongoClientURI connectionString = new MongoClientURI(System.getenv("MONGO_DB_URI"));
+            MongoClientURI connectionString = new MongoClientURI("mongodb://test:test@ds031975.mlab.com:31975/ads"/*System.getenv("MONGO_DB_URI")*/);
             MongoClient mongoClient = new MongoClient(connectionString);
 
             Morphia morphia = new Morphia();
             morphia.mapPackage("model");
-            datastore = morphia.createDatastore(mongoClient, System.getenv("MONGO_DB_NAME"));
+            datastore = morphia.createDatastore(mongoClient,"ads" /*System.getenv("MONGO_DB_NAME")*/);
             datastore.ensureIndexes();
         }
         return datastore;
